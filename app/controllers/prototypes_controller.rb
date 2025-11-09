b4c8eae944af34a8083b089b6e498f8e61b2e4ce
ryouma21 <zuotenglengzhen58@gmail.com>
2025-11-09 22:22:49 +0900

    投稿者以外が編集・削除できないようにアクセス制限を追加

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_prototype, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @prototypes = Prototype.all
